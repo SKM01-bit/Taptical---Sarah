e1e167ff5596ffe72412c161e27d1d56c5c7b05e
Sarah <Sarahalsarah77@gmail.com>
2025-09-25 11:03:42 +0300

    Initial Commit

---
+++ Taptical - Sarah/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Taptical - Sarah
+//
+//  Created by Sarah Khalid Almalki on 03/04/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
