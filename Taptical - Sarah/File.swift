1277ccbdc6b83d8e167c9a7e2b016617259edb7d
Sarah <Sarahalsarah77@gmail.com>
2025-09-30 11:40:55 +0300

    change

---
+++ Taptical - Sarah/File.swift
@@ -0,0 +1,1 @@
+     //   Image(isTapped ?"شعاع":"taptii")
---
+++ Taptical - Sarah/FourthPage.swift
@@ -0,0 +1,18 @@
+//
+//  FourthPage.swift
+//  Taptical - Sarah
+//
+//  Created by Sarah Khalid Almalki on 08/04/1447 AH.
+//
+
+import SwiftUI
+
+struct FourthPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FourthPage()
+}
