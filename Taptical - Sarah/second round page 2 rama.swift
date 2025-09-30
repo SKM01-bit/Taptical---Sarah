3361915588229a26f38caddeb8050b4b69a381f4
Rama <ramabader891919@gmail.com>
2025-09-30 02:29:44 -0700

    add new

---
+++ Taptical - Sarah/second round page 2 rama.swift
@@ -0,0 +1,18 @@
+//
+//  second round page 2 rama.swift
+//  Taptical - Sarah
+//
+//  Created by راما بدر بن جامع on 08/04/1447 AH.
+//
+
+import SwiftUI
+
+struct second_round_page_2_rama: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    second_round_page_2_rama()
+}
