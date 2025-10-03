82f44916af969e47e261a14d7127a1b49ccfbb33
Jana Malibari <Konderyano.0o0@hotmail.com>
2025-10-03 20:51:31 +0300

    Round 4 Push

---
+++ Taptical - Sarah/Round4.swift
@@ -0,0 +1,18 @@
+//
+//  Round4.swift
+//  Taptical - Sarah
+//
+//  Created by Jana Abdulaziz Malibari on 03/10/2025.
+//
+
+import SwiftUI
+
+struct Round4: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Round4()
+}
