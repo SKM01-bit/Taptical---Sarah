e97278823afabb4173db21a37e55c6655386d45d
najla <najlaalmuqati1@gmail.com>
2025-10-05 09:40:52 +0300

    FirstRound

---
+++ Taptical - Sarah/ROUND1.swift
@@ -0,0 +1,18 @@
+//
+//  ROUND1.swift
+//  Taptical - Sarah
+//
+//  Created by Najla Almuqati on 13/04/1447 AH.
+//
+
+import SwiftUI
+
+struct ROUND1: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ROUND1()
+}
