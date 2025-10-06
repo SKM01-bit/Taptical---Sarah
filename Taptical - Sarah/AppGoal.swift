7583d4337f120e6946672a530d22210df70aff80
Jana Malibari <Konderyano.0o0@hotmail.com>
2025-10-06 16:41:08 +0300

    Semi-Fianl Draft

---
+++ Taptical - Sarah/AppGoal.swift
@@ -0,0 +1,18 @@
+//
+//  AppGoal.swift
+//  Taptical - Sarah
+//
+//  Created by Jana Abdulaziz Malibari on 06/10/2025.
+//
+
+import SwiftUI
+
+struct AppGoal: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppGoal()
+}
